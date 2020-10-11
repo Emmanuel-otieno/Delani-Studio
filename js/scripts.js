f019b1807d36420e446a9272cc08f2168485ba6c
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-11 19:41:35 +0300

    add toggle

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("DEVELOPMENT").click(function(){
+      $("p").toggle();
+    });
+
+    $("PRODUCT MANAGEMENT").click(function(){
+        $("p").toggle();
+    });
+
+    $("DESING").click(function(){
+        $("p").toggle();
+    });
+
+
+});
