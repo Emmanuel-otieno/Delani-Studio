a5c04e0f82d4f4740928f1cb45b9a8788973d292
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-12 11:34:37 +0300

    add a hover

--- js/scripts.js
+++ js/scripts.js
@@ -28,11 +28,59 @@ function contactUs(){
  }
 }
 
-$('.col').hover(function(){
+$(document).ready(function () {
+    $("#omg1").mouseover(function () {
+            $("#work1").show();
+        })
+        .mouseout(function () {
+            $("#work1").hide();
+        });
+
+  $("#omg2").mouseover(function () {
+            $("#work2").show();
+        })
+        .mouseout(function () {
+            $("#work2").hide();
+        });
+
+   $("#omg3").mouseover(function () {
+            $("#work3").show();
+        })
+        .mouseout(function () {
+            $("#work3").hide();
+        });
+  $("#omg4").mouseover(function () {
+            $("#work4").show();
+        })
+        .mouseout(function () {
+            $("#work4").hide();
+        });
+   $("#omg5").mouseover(function () {
+            $("#work5").show();
+        })
+        .mouseout(function () {
+                 $("#work5").hide();
+    $("#omg6").mouseover(function () {
+                $("#work6").show();
+            })
+            .mouseout(function () {
+                $("#work6").hide();
+            });
+    $("#omg7").mouseover(function () {
+                $("#work7").show();
+            })
+            .mouseout(function () {
+                $("#work7").hide();
+            });
+     $("#omg8").mouseover(function () {
+                $("#work8").show();
+            })
+            .mouseout(function () {
+                $("#work8").hide();
+            });
+    
+
+ });
 
 
-
-
-
-
-});
+ });
