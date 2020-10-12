09fd76e98492398d48a595e54d284ecf498931de
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-12 07:50:34 +0300

    add a function

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,12 @@ $(document).ready(function(){
 
 });
 function contactUs(){
-    
+    var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+   return false;
+  }
+  else{
+      alert("" +"we have received your message" + "Thank you fro reaching out to us");
+  }
 }
